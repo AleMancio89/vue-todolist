46e57bce0f797fc5da68e75d7ea61ea9cad77d8a
Alessandro <alessandro.mancinelli89@gmail.com>
2020-11-19 17:41:18 +0100

    Added highlight function and minor fixes

--- js/main.js
+++ js/main.js
@@ -1,17 +1,32 @@
+/*
+Creare una web app con Vue che permetta ad un utente di:
+Inserire un elemento nella lista
+Rimuovere un elemento dalla lista
+*/
+
 const myApp = new Vue({
   el: '#root',
   data: {
     userInput:'',
-    toDoes: [],
-    isActive: true
+    toDos: []
   },
   methods: {
     addToDo: function() {
-      this.toDoes.push(this.userInput);
+      // Aggiungo all'array oggetti contenenti l'input dell'utente e lo stato iniziale per l'highlight
+      this.toDos.push({
+        text: this.userInput,
+        active: false
+      });
+      //Ripulisco il campo dell'input dopo aver salvato il valore nell'array
       this.userInput = '';
     },
     removeItem: function(i) {
-      this.toDoes.splice(i, 1);
+      //Rimuovo dall'array l'oggetto che desidero
+      this.toDos.splice(i, 1);
+    },
+    highlightItem: function(index){
+      //Vario lo stato del campo active dell'array
+      this.toDos[index].active = !(this.toDos[index].active)
     }
   }
 });
